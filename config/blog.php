da907b612d1daf0fc6987ba456a0f3a117629ebb
kempers <alex@kapital18.ru>
2016-09-04 19:07:32 +0300

    Демо блог на Laravel5
    Создан исключительно в ознакомительных целях. Дальнейшее развитие не
    планируется.

---
+++ config/blog.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sitdikov_aa
+ * Date: 16.07.2016
+ * Time: 21:06
+ */
+return [
+    'title' => 'Тестовый Блог автоматический заполненный демо-данными',
+    'posts_per_page' => 10
+];
